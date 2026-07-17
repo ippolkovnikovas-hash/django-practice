39c60075c7e064b6053e473f70f035b1a8a82552
Aleksandr <ippolkovnikov@mail.ru>
2026-07-17 17:07:44 +0300

    Add request data app and throttling setup

---
+++ mysite/requestdataapp/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+def process_get_view(request:HttpRequest) -> HttpResponse:
+    a = request.GET.get("a", "")
+    b = request.GET.get("b", "")
+    result = a + b
+    context = {
+       "a": a,
+        "b": b,
+        "result": result,
+    }
+    return render(request, "requestdataapp/request-query-params.html", context=context)
