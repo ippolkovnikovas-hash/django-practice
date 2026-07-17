1e01a9a34a9ac38ee5d6d874894ce05fceadc606
Aleksandr <ippolkovnikov@mail.ru>
2026-07-17 13:21:30 +0300

    fix(shopapp): исправить discount на PositiveSmallIntegerField, добавить навигацию и убрать дублирующий include

--- DatabaseAndModels/mysite/shopapp/models.py
+++ DatabaseAndModels/mysite/shopapp/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-    discount = models.SmallIntegerField(default=0)
+    discount = models.PositiveSmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
 
