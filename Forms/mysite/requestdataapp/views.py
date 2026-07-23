6271f9bef9ca2076f8411456a8fe93055157dc58
Aleksandr <ippolkovnikov@mail.ru>
2026-07-23 10:59:21 +0300

    Add forms and middleware to requestdataapp

--- Forms/mysite/requestdataapp/views.py
+++ Forms/mysite/requestdataapp/views.py
@@ -1,29 +1,43 @@
+from multiprocessing import context
+
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .forms import UserBioForm, UploadFileForm
 
-def process_get_view(request: HttpRequest) -> HttpResponse:
+def process_get_view(request:HttpRequest) -> HttpResponse:
     a = request.GET.get("a", "")
     b = request.GET.get("b", "")
     result = a + b
     context = {
-        "a": a,
+       "a": a,
         "b": b,
         "result": result,
     }
     return render(request, "requestdataapp/request-query-params.html", context=context)
 
-
 def user_form(request: HttpRequest) -> HttpResponse:
-    return render(request, "requestdataapp/user-bio-form.html")
-
+    context = {
+        "form": UserBioForm(),
+    }
+    return render(request, "requestdataapp/user-bio-form.html", context=context)
 
 def handle_file_upload(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST" and request.FILES.get("myfile"):
-        myfile = request.FILES["myfile"]
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        print("saved file", filename)
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
 
-    return render(request, "requestdataapp/file-upload.html")
+            #myfile = request.FILES["myfile"]
+            myfile = form.cleaned_data["file"]
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            print("saved file", filename)
+    else:
+        form = UploadFileForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "requestdataapp/file-upload.html", context=context)
